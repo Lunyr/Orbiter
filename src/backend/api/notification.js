d25d0dfbd921b2eaeb714dfa5fc45928951027ed
Mike Shultz <mike@lunyr.com>
2018-07-20 13:40:11 -0600

    Notification data API

---
+++ src/backend/api/notification.js
@@ -0,0 +1,67 @@
+import { db } from '../db';
+import settings from '../shared/settings';
+import { web3 } from '../shared/web3';
+
+const addNotification = async (address, type, dataObj) => {
+  try {
+    const data = await db('notification').insert({
+      hashed_address: web3.sha3(address),
+      type,
+      data: dataObj
+    });
+    console.log("addNotification insert result", data);
+    return {
+      success: true,
+      data,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message,
+    };
+  }
+};
+
+const getNotifications = async (address) => {
+  try {
+    const data = await db('notification').where({
+      hashed_address: web3.sha3(address),
+    }).select();
+    console.log("getNotifications select result", data);
+    return {
+      success: true,
+      data,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message,
+    };
+  }
+};
+
+const markRead = async (notification_id) => {
+  try {
+    const data = await db('notification').where({
+      notification_id
+    }).update({
+      read: true
+    });
+    console.log("markRead update result", data);
+    return {
+      success: true,
+      data,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message,
+    };
+  }
+};
+
+export default {
+  addNotification,
+  getNotifications,
+  markRead,
+};
