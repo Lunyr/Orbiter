302d9cfdfca1eb3d51dc8bb13daa3150d3cfa4fe
Mike Shultz <mike@lunyr.com>
2018-08-01 16:16:27 -0600

    Fixed bad indent for backend/api exports

--- src/backend/api/index.js
+++ src/backend/api/index.js
@@ -3,57 +3,57 @@ export { addTx, getAddressByTx } from './transaction';
 export { getWatch, getPendingWatch, addWatch, setWatchState } from './watch';
 export { addEvent } from './event';
 export {
-    addNotification,
-    getNotifications,
-    markRead,
-    getUnread
+  addNotification,
+  getNotifications,
+  markRead,
+  getUnread
 } from './notification';
 export {
-    getEditStream,
-    addEditStream,
-    updateEditStream
+  getEditStream,
+  addEditStream,
+  updateEditStream
 } from './editstream';
 export {
-    getProposal,
-    getDirtyProposals,
-    addProposal,
-    updateProposal,
-    rejectProposal,
-    acceptProposal,
-    expireProposal,
-    getProposalsWrittenBy,
-    getProposalsInReviewBy,
-    getProposalsInReview,
+  getProposal,
+  getDirtyProposals,
+  addProposal,
+  updateProposal,
+  rejectProposal,
+  acceptProposal,
+  expireProposal,
+  getProposalsWrittenBy,
+  getProposalsInReviewBy,
+  getProposalsInReview,
 } from './proposal';
 export {
-    getArticles,
-    getCurrentArticle,
-    getCurrentArticleByTitle,
-    getContributors,
+  getArticles,
+  getCurrentArticle,
+  getCurrentArticleByTitle,
+  getContributors,
 } from './article';
 export {
-    getVote,
-    getDirtyVotes,
-    updateVote,
-    addVote,
-    getProposalVoteStats,
-    getProposalVotes,
-    userVotedOnProposal,
-    getUsersRecentVotes,
+  getVote,
+  getDirtyVotes,
+  updateVote,
+  addVote,
+  getProposalVoteStats,
+  getProposalVotes,
+  userVotedOnProposal,
+  getUsersRecentVotes,
 } from './vote';
 export {
-    getTag,
-    getTags,
-    addTag,
-    activateTag,
-    addTagProposal,
-    associateTag,
-    getTagAssociation
+  getTag,
+  getTags,
+  addTag,
+  activateTag,
+  addTagProposal,
+  associateTag,
+  getTagAssociation
 } from './tag';
 export {
-    getDraft,
-    getDraftByProposalId,
-    setDraftToDraft,
-    addDraft,
-    editDraft,
+  getDraft,
+  getDraftByProposalId,
+  setDraftToDraft,
+  addDraft,
+  editDraft,
 } from './draft';
